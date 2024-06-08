07b3bc8e0749dee560a8dfc0116406647eb9fcba
poojapdarade <poojap.darade@gmail.com>
2024-06-08 23:28:05 +0100

    Making contact page in react

--- react-version/src/main.jsx
+++ react-version/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage.jsx";
 import { Articles } from "./pages/Articles.jsx";
+import { Contact } from "./pages/contact.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/articles" element={<Articles />} />
+        <Route path="/contact" element={Contact} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ react-version/src/pages/contact.jsx
@@ -0,0 +1,7 @@
+export function Contact() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
