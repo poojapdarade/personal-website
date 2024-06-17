ec31b8c396156d73d4ccef6feb0c4d00fe732c3f
poojapdarade <poojap.darade@gmail.com>
2024-06-17 16:12:13 +0100

    Minimum number game added to react-version

--- react-version/src/pages/coding-puzzles/MinimumNumberGame.jsx
+++ react-version/src/pages/coding-puzzles/MinimumNumberGame.jsx
@@ -1,10 +1,56 @@
 import React, { useState } from "react";
 export function MinimumNumberGame() {
-  const [count, setCount] = useState(0);
+  const [playArray, setPlayArray] = useState([]);
+  const [gameArray, setGameArray] = useState([]);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  function reset() {
+    setPlayArray([]);
+    setGameArray([]);
+  }
+
+  function addNumbers() {
+    const newPlayArray = [...playArray];
+    newPlayArray.push(number1);
+    newPlayArray.push(number2);
+    setPlayArray(newPlayArray);
+  }
+
+  function playTheGame() {
+    const newGameArray = [...playArray];
+    const ascendingArray = newGameArray.toSorted((a, b) => a - b);
+    const output = [];
+    for (let i = 0; i < ascendingArray.length; i += 2) {
+      const alice = ascendingArray[i];
+      const bob = ascendingArray[i + 1];
+      output.push(bob);
+      output.push(alice);
+    }
+
+    setGameArray(output);
+  }
+
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p>Add Numbers</p>
+
+      <input
+        type="number"
+        value={number1}
+        onChange={(event) => setNumber1(Number(event.target.value))}
+      />
+      <input
+        type="number"
+        value={number2}
+        onChange={(event) => setNumber2(Number(event.target.value))}
+      />
+      <button onClick={addNumbers}>Add Numbers</button>
+      <p>Numbers in the array are : {playArray}</p>
+
+      <button onClick={playTheGame}>Play</button>
+      <p>The output array is: {gameArray}</p>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
